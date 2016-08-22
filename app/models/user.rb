b965c3b33f4bc32b001170298d0f203ae486d56a
SA <stankra@yandex.ru>
2016-08-22 08:54:36 +0300

    Add Chatroom, ChatroomUser, Message models & Chatroom controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :chatroom_users
+  has_many :messages
+  has_many :chatrooms, through: :chatroom_users
+
   validates :password, length: { minimum: 6 },      if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true,          if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  root to: 'chatrooms#index'
+  resources :chatrooms
   resources :users
   resources :user_sessions, only: [:new, :create, :destroy]
 
-  get 'login' => 'user_sessions#new', as: :login
+  get  'login'  => 'user_sessions#new',     as: :login
   post 'logout' => 'user_sessions#destroy', as: :logout
 end
