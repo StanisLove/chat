a88b1825e25e54bc92e12f06f2c9d891ededc66d
SA <stankra@yandex.ru>
2016-08-30 20:51:38 +0300

    Add ChatroomsChannel

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      logger.add_tags "ActionCable", "User #{current_user.id}"
+    end
+
+    protected
+
+      def find_verified_user
+        env['rack.session'].send(:load_for_read!)
+
+        if id = env['rack.session']['user_id']
+          User.find(id)
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
