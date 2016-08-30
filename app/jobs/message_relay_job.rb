6520392cd4f769d0af5f4866568c0e75252417c3
SA <stankra@yandex.ru>
2016-08-30 21:57:44 +0300

    Fix mistake

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -2,8 +2,8 @@ class MessageRelayJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}", {
-      message: MessageController.render(message),
+    ActionCable.server.broadcast "chatroom:#{message.chatroom.id}", {
+      message: MessagesController.render(message),
       chatroom_id: message.chatroom.id
     }
   end
